363234e283c4b1ac2ae53d82501626ac74ff31b7
comp500 <comp500@users.noreply.github.com>
2022-06-17 19:42:55 +0100

    Build HTML file in dist folder

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -3,6 +3,7 @@ const stream = require("stream").promises;
 const TOML = require("@ltd/j-toml");
 const { createWriteStream } = require("fs");
 const { Readable } = require("stream");
+const { html, render } = require("ucontent");
 
 async function* getMods() {
 	for (const ent of await fs.readdir("src/mods", { withFileTypes: true })) {
@@ -51,7 +52,67 @@ function compareBool(a, b) {
 	}
 }
 
-async function* generateReadme() {
+async function collect(iter) {
+	const res = [];
+	for await (const item of iter) {
+		res.push(item);
+	}
+	return res;
+}
+
+async function* generateModsTable(modsList, mods) {
+	yield html`
+<table>
+${modsList.map(mod => {
+			let tags = [];
+			if (mod.notes !== undefined) {
+				tags.push(mod.notes);
+			}
+			if (mod.metadata !== undefined && mod.metadata.outdated) {
+				tags.push("outdated");
+			}
+			if (mod.metadata !== undefined && mod.metadata.recommended_client) {
+				tags.push("works best with client mod installed");
+			}
+			if (mod.metadata !== undefined && mod.metadata.experimental) {
+				tags.push("experimental");
+			}
+			if (mod.metadata !== undefined && mod.metadata.abandoned) {
+				tags.push("abandoned");
+			}
+			if (mod.upstream !== undefined) {
+				tags.push("fork of " + mods[mod.upstream].name)
+			}
+
+			let links = [];
+			if (mod.links.modrinth !== undefined) {
+				links.push(html`<a href=${mod.links.modrinth}>Modrinth</a>`);
+				links.push(" ");
+			}
+			if (mod.links.curseforge !== undefined) {
+				links.push(html`<a href=${mod.links.curseforge}>CurseForge</a>`);
+				links.push(" ");
+			}
+			if (mod.links.github !== undefined) {
+				links.push(html`<a href=${mod.links.github}>Github</a>`);
+				links.push(" ");
+			}
+			if (mod.links.website !== undefined) {
+				links.push(html`<a href=${mod.links.website}>Website</a>`);
+				links.push(" ");
+			}
+
+			return html`<tr>
+	<td>${mod.name}</td>
+	<td>${links.slice(0, -1)}</td>
+	<td>${tags.join(", ")}</td>
+</tr>
+`
+		})}
+</table>`;
+}
+
+async function* generateModsTables() {
 	const mods = {};
 	const modsList = [];
 	for await (const [slug, mod] of getMods()) {
@@ -73,78 +134,43 @@ async function* generateReadme() {
 		return cmp;
 	});
 
-	yield `# Quilt Server-side Mods
-
-This is a list of server-side mods for the Quilt modloader; including many Fabric mods (which are compatible with Quilt) and some Quilt-only mods. Wondering what Quilt is? It's a new modloader, compatible with the vast majority of Fabric mods; see [the FAQ page here](https://quiltmc.org/about/faq/)!
-Feel free to submit a Pull Request if you find a server side Quilt/Fabric mod not listed here.
-
-Also see [Optifine Alternatives](https://lambdaurora.dev/optifine_alternatives/) for a few useful client side only mods!
-
-Mods on this list are marked as outdated when they are *two* major Minecraft versions old - e.g. if 1.16 is the latest version, 1.14 and older mods are considered outdated.
-
-`;
-
 	for await (const [slug, category] of getCategories()) {
-		yield "#".repeat((slug.match(/\//g)||[]).length + 2) + " " + category.name;
-		yield "\n";
+		switch ((slug.match(/\//g)||[]).length) {
+			case 0:
+				yield html`<h2>${category.name}</h2>`
+				break;
+			case 1:
+				yield html`<h3>${category.name}</h3>`
+				break;
+			default:
+				yield html`<h4>${category.name}</h4>`
+				break;
+		}
 		if (category.notes !== undefined) {
-			yield category.notes;
-			yield "\n\n";
+			yield html`<p>${category.notes}</p>`;
 		}
 
-		let firstMod = true;
-
-		for (const mod of modsList) {
-			if (mod.categories.includes(slug)) {
-				if (firstMod) {
-					yield "|Name|Links|Notes|\n";
-					yield "|-|-|-|\n";
-					firstMod = false;
-				}
-
-				let tags = [];
-				if (mod.notes !== undefined) {
-					tags.push(mod.notes);
-				}
-				if (mod.metadata !== undefined && mod.metadata.outdated) {
-					tags.push("outdated");
-				}
-				if (mod.metadata !== undefined && mod.metadata.recommended_client) {
-					tags.push("works best with client mod installed");
-				}
-				if (mod.metadata !== undefined && mod.metadata.experimental) {
-					tags.push("experimental");
-				}
-				if (mod.metadata !== undefined && mod.metadata.abandoned) {
-					tags.push("abandoned");
-				}
-				if (mod.upstream !== undefined) {
-					tags.push("fork of " + mods[mod.upstream].name)
-				}
-
-				let links = [];
-				if (mod.links.modrinth !== undefined) {
-					links.push(`[Modrinth](${mod.links.modrinth})`);
-				}
-				if (mod.links.curseforge !== undefined) {
-					links.push(`[CurseForge](${mod.links.curseforge})`);
-				}
-				if (mod.links.github !== undefined) {
-					links.push(`[Github](${mod.links.github})`);
-				}
-				if (mod.links.website !== undefined) {
-					links.push(`[Website](${mod.links.website})`);
-				}
-
-				yield `|${mod.name}|${links.join(" ")}|${tags.join(", ")}|\n`;
-			}
+		let categoryMods = modsList.filter(mod => mod.categories.includes(slug));
+		if (categoryMods.length > 0) {
+			yield* generateModsTable(categoryMods, mods);
 		}
-
-		yield "\n";
 	}
 }
 
+async function generateReadme() {
+	return html`
+<h1>Quilt Server-side Mods</h1>
+<p>This is a list of server-side mods for the Quilt modloader; including many Fabric mods (which are compatible with Quilt) and some Quilt-only mods. Wondering what Quilt is? It&#39;s a new modloader, compatible with the vast majority of Fabric mods; see <a href="https://quiltmc.org/about/faq/">the FAQ page here</a>!
+Feel free to submit a Pull Request if you find a server side Quilt/Fabric mod not listed here.</p>
+<p>Also see <a href="https://lambdaurora.dev/optifine_alternatives/">Optifine Alternatives</a> for a few useful client side only mods!</p>
+<p>Mods on this list are marked as outdated when they are <em>two</em> major Minecraft versions old - e.g. if 1.16 is the latest version, 1.14 and older mods are considered outdated.</p>
+
+${await collect(generateModsTables())}
+	`;
+}
+
 (async () => {
-	const readmeStream = createWriteStream("README.md");
-	stream.pipeline(generateReadme(), readmeStream);
+	await fs.mkdir("dist", { recursive: true });
+	const readmeStream = createWriteStream("dist/index.html");
+	render(readmeStream, await generateReadme()).end();
 })();
